f053abfc4de04da6264744c369eed93b448d6257
tippettj <tippettj@gmail.com>
2020-09-11 11:12:37 -0700

    Initial Commit

---
+++ app.js
@@ -0,0 +1,69 @@
+const chalk = require("chalk");
+const yargs = require("yargs");
+const notes = require("./notes.js");
+
+yargs.version("1.1.0");
+
+yargs.command({
+  command: "add",
+  describe: "Add a new note",
+  builder: {
+    title: {
+      describe: "Note title",
+      demandOption: true,
+      type: "string",
+    },
+    body: {
+      describe: "Note Body",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler(argv) {
+    notes.addNote(argv.title, argv.body);
+  },
+});
+
+// yargs.command("test", "my test", function (yargs) {
+//   console.log("Testing a new note!");
+// });
+
+yargs.command({
+  command: "remove",
+  describe: "Remove a note",
+  builder: {
+    title: {
+      describe: "Note title",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler(argv) {
+    notes.removeNote(argv.title);
+  },
+});
+
+yargs.command({
+  command: "list",
+  describe: "List a note",
+  handler() {
+    notes.listNotes();
+  },
+});
+
+yargs.command({
+  command: "read",
+  describe: "Read a note",
+  builder: {
+    title: {
+      describe: "Note title",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler(argv) {
+    notes.readNote(argv.title);
+  },
+});
+
+yargs.parse();
